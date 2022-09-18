c9f42a4bd71b749d44a96323a703a2b602494a38
vyntt <vyntt@inspirelab.io>
2022-09-18 09:32:44 +0700

    feat(backend): deploy

--- Makefile
+++ Makefile
@@ -2,3 +2,13 @@ protoGo:
 	protoc --proto_path=backend/api --go_out=backend/api/pb --go_opt=paths=source_relative --go-grpc_out=backend/api/pb --go-grpc_opt=paths=source_relative backend/api/calendar.proto
 protoTs:
 	protoc -I=./backend/api --js_out=import_style=commonjs:./backend/api/pb --grpc-web_out=import_style=typescript,mode=grpcwebtext:./backend/api/pb backend/api/calendar.proto
+
+deploy:
+	docker compose down
+	docker compose build
+	docker compose up
+	# docker tag 4cbc4484f6da registry.heroku.com/dry-oasis-02282/web
+push-deploy:
+	docker push registry.heroku.com/dry-oasis-02282/web
+	heroku container:release web --app=dry-oasis-02282
+	heroku logs --tail
--- backend/internal/database/database.go
+++ backend/internal/database/database.go
@@ -4,27 +4,28 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 	"remy/pkg/ent"
 )
 
 func Init() (*ent.Client, error) {
 	// Connect database
-	DB_HOST := os.Getenv("DB_HOST")
-	DB_OUT_PORT := "3306"
-	DB_IN_PORT := "3306"
-	DB_USER := "root"
-	DB_PASS := "P@ssw0rd"
-	DB_NAME := "remy"
+	DB_HOST := "ec2-34-200-205-45.compute-1.amazonaws.com"
+	DB_OUT_PORT := "5432"
+	DB_IN_PORT := "5432"
+	DB_USER := "ethidrlsvjpwss"
+	DB_PASS := "7d04e57ffe09ea310e3838082ae4a4fa17948af82fb690e73a203780eccb5ab4"
+	DB_NAME := "df6687mil3q4ar"
 	fmt.Printf("db_out: %s, db_in: %s\n", DB_OUT_PORT, DB_IN_PORT)
 	fmt.Printf("Connecting to Mysql: %s:%s@tcp(%s:%s)/%s?parseTime=True\n", DB_USER, DB_PASS, DB_HOST, DB_OUT_PORT, DB_NAME)
-	database, errDB := ent.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True", DB_USER, DB_PASS, DB_HOST, DB_OUT_PORT, DB_NAME))
+	// postgres://ethidrlsvjpwss:7d04e57ffe09ea310e3838082ae4a4fa17948af82fb690e73a203780eccb5ab4@ec2-34-200-205-45.compute-1.amazonaws.com:5432/df6687mil3q4ar
+	database, errDB := ent.Open("postgres", fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", DB_HOST, DB_OUT_PORT, DB_USER, DB_NAME, DB_PASS))
 	fmt.Println("Connect to Database successfully!")
 	if errDB != nil {
 		return nil, errDB
 	}
 	// Run the auto migration tool.
 	if errSchema := database.Schema.Create(context.Background()); errSchema != nil {
+		log.Fatalf("failed creating schema resources: %v", errSchema)
 		log.Fatalf("failed creating schema resources: %v", errSchema)
 		return nil, errSchema
 	}
